ccae0496b4d761e429ee7d3fccc9e744e80cb400
gerald-annan <geraldannan400@gmail.com>
2022-01-01 13:38:44 +0000

    configures baseUrl from scraperapi_url and api_key

---
+++ lib/torrent/client.ex
@@ -0,0 +1,9 @@
+defmodule Torrent.Client do
+  @scraperapi_url Application.get_env(:torrent_client, :scraper_api)
+  @api_key Application.get_env(:torrent_client, :api_key)
+  @baseUrl "#{@scraperapi_url}api_key=#{@api_key}&autoparse=true"
+
+  def fetch() do
+    @baseUrl
+  end
+end
