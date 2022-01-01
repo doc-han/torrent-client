76acb1ff0f80a63c4efe162a806f819930f87241
gerald-annan <geraldannan400@gmail.com>
2022-01-01 14:30:49 +0000

    combines request params with baseUrl

--- lib/torrent/client.ex
+++ lib/torrent/client.ex
@@ -2,8 +2,15 @@ defmodule Torrent.Client do
   @scraperapi_url Application.get_env(:torrent_client, :scraper_api)
   @api_key Application.get_env(:torrent_client, :api_key)
   @baseUrl "#{@scraperapi_url}api_key=#{@api_key}&autoparse=true"
+  @torrentUrl "https://pirate-bays.net/search?q="
 
   def fetch(query) do
-    @baseUrl
+    String.split(query)
+    |> Enum.join("+")
+    |> request()
+  end
+
+  def request(params) do
+    @baseUrl <> "&url=" <> @torrentUrl <> params
   end
 end
