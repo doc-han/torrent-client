7b3482852b32fbc650b768ae8da69a54ed5ed9f7
gerald-annan <geraldannan400@gmail.com>
2022-01-01 13:14:20 +0000

    adds configurations for scraper api_key and scraper url

---
+++ config/config.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+config :torrent_client, api_key: "39ada2fa714a20c88ba24995f71a89c5"
+config :torrent_client, scraper_api: "https://api.scraperapi.com?"
+config :logger, compile_time_purge_info: :info
