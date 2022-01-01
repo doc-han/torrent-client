cb6c897f27223fe075501cea23a62f2c11849e24
gerald-annan <geraldannan400@gmail.com>
2022-01-01 13:15:04 +0000

    adds github source to project structure

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule TorrentClient.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      source: "https://github.com/gerald-annan/torrent-client",
       deps: deps()
     ]
   end
