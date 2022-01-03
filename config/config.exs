3d77a77fc3205a2bf55f905a1656b896e4fc4b98
gerald-annan <geraldannan400@gmail.com>
2022-01-03 20:12:11 +0000

    Cleaned up client configuration

--- config/config.exs
+++ config/config.exs
@@ -2,5 +2,5 @@ use Mix.Config
 config :torrent_client, api_key: "39ada2fa714a20c88ba24995f71a89c5"
 config :torrent_client, scraper_api: "https://api.scraperapi.com?"
 config :logger, compile_time_purge_info: :info
-config :torrent_client, ytx_api: "https://yts.mx/api/v2/"
+config :torrent_client, yts_api: "https://yts.mx/api/v2/"
 config :torrent_client, headers: ["id", "title_long", "torrents", "summary"]
